ef0b3db28d64c9d440461df8c6979c4d337ddd22
Michael Weedman <michael.weedman@gmail.com>
2019-11-26 11:12:20 -0700

    completed functionality

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-'use strict';
+"use strict";
 
-const baseURL = 'https://api.github.com/search/users?q='
+const baseURL = "https://api.github.com/users/";
 
-const renderRepos = (repoData)  => {
-    console.log(repoData);
-    repoData.items.map(item => (console.log(item.repos_url)))
-}
+const renderRepos = repoData => {
+  console.log(repoData);
+  repoData.map(repo => {
+    $(".results").append(repoHtml(repo));
+  });
+};
 
-const apiFetch = (searchHandle) => {
-    const newURL = `${baseURL}${searchHandle}`
-    console.log(newURL);
-    return fetch(newURL)
-        .then(res => {
-            if(!res.ok) {
-                alert('No user found.')
-            }
-            return res;
-        })
-        .then(res => res.json())
-        .then(data => renderRepos(data));
-}
+const repoHtml = repo => {
+  return `
+    <h2>${repo.name}</h2>
+    <h6>${repo.url}</h6>
+    `;
+};
+
+const apiFetch = searchHandle => {
+  const newURL = `${baseURL}${searchHandle}/repos`;
+  console.log(newURL);
+  return fetch(newURL)
+    .then(res => {
+      if (!res.ok) {
+        alert("No user found.");
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .then(data => renderRepos(data));
+};
 
 const submitForm = function() {
-    $('#inputForm').submit( (e) => {
-        e.preventDefault();
-        $('.results').empty();
-        const userToSearch = $('#userSearch').val();
-        console.log(userToSearch);
-        apiFetch(userToSearch);
-    });
-}
+  $("#inputForm").submit(e => {
+    e.preventDefault();
+    $(".results").empty();
+    const userToSearch = $("#userSearch").val();
+    console.log(userToSearch);
+    apiFetch(userToSearch);
+  });
+};
 
 const bindListeners = () => {
-    submitForm();
-}
+  submitForm();
+};
 
-const main = function(){
-    bindListeners();
-}
+const main = function() {
+  bindListeners();
+};
 
-$(main)
+$(main);
